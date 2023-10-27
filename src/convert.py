0921a31f9de9f8e4b84f88b2d40f16f3396665ff
palewire <b@palewi.re>
2023-10-27 11:57:29 -0400

    Sure

--- src/convert.py
+++ src/convert.py
@@ -1,10 +1,16 @@
 """Convert raw NOAA data into GeoJSON files."""
 from __future__ import annotations
 
+import json
 import shutil
+import subprocess
 from pathlib import Path
+import warnings
 
 import click
+import geopandas as gpd
+
+from . import utils
 
 THIS_DIR = Path(__file__).parent
 RAW_DIR = THIS_DIR.parent / "data" / "raw"
@@ -34,6 +40,37 @@ def convert() -> None:
         print(f"Copying {item_path} to {processed_path}")
         shutil.copy(item_path, processed_path)
 
+        # Get a list of all files in the raw directory with glob
+        raw_files = this_raw_dir.glob("*")
+
+        # Loop through the files:
+        for f in raw_files:
+            # If it's a .shp file ...
+            if str(f).endswith(".shp"):
+                # Read it in with geopandas
+                gdf = gpd.read_file(str(f))
+
+                # Set the output path for a geojson output
+                geojson_path = this_processed_dir / f"{f.stem}.geojson"
+
+                # Ignore any UserWarnings
+                with warnings.catch_warnings():
+                    warnings.simplefilter("ignore")
+                    # Convert the geopandas dataframe to geojson
+                    json_data = gdf.to_json(indent=4)
+
+                # Write it out
+                utils.write_json(json.loads(json_data), geojson_path)
+
+            # If it's a KML file
+            elif str(f).endswith(".kmz"):
+                # Set the output path for a geojson output
+                geojson_path = this_processed_dir / f"{f.stem}.geojson"
+
+                # Convert it with ogr2ogr
+                print(f"Converting {f} to {geojson_path}")
+                command = ['ogr2ogr', '-f', 'GeoJSON', str(geojson_path), str(f)]
+                subprocess.run(command)
 
 
 
